8c8288e9f2378bdf9f623be75a6c3e5f913bc2e4
Tchoko Apps <tchokoapps@gmail.com>
2019-05-25 14:59:02 +0200

    first version

---
+++ src/main/java/com/tchokoapps/springboot/spring5webappdemo/Spring5webappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tchokoapps.springboot.spring5webappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tchokoapps/springboot/spring5webappdemo/Spring5webappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchokoapps.springboot.spring5webappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
